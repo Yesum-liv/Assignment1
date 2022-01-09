4c1ca27a0801c3e039cefc0c29af62cd20133247
Yesum-liv <snehalkarki4959@gmail.com>
2022-01-09 22:26:51 +0545

    Added parallel combination

--- Resistance.c
+++ Resistance.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 void main()
 {
-    float R1,R2,Rt,Parallel,Series;
+    float R1, R2, Rt, Parallel, Series;
     printf("Enter the value of first resistance:");
-    scanf("%f",&R1);
+    scanf("%f", &R1);
     printf("Enter the value of second resistance:");
-    scanf("%f",&R2);
-    Series= R1+R2;
-    printf("The value of equilavent resistance in series is %f",Series);
+    scanf("%f", &R2);
+    Series = R1 + R2;
+    Parallel = (R1 * R2) / (R1 + R2);
+    printf("The value of equilavent resistance in series is %f", Series);
+    printf("The value of equilavent resistance in parallel is %f", Parallel);
+
     getch();
-    
 }
